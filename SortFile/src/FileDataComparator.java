1b35c2a7a7592314f0b3fca93a4c90434c2f2e8e
Paulo Ayala <pca488s@yahoo.com>
2017-11-20 10:35:53 -0600

    Add remaining fields to Comparator.

--- SortFile/src/FileDataComparator.java
+++ SortFile/src/FileDataComparator.java
@@ -32,6 +32,20 @@ public class FileDataComparator implements Comparator<FileData> {
 					currentScore = o1.address.compareTo(o2.address);
 				} else if(field.equals("city")){
 					currentScore = o1.city.compareTo(o2.city);
+				} else if(field.equals("country")){
+					currentScore = o1.country.compareTo(o2.country);
+				} else if(field.equals("state")){
+					currentScore = o1.state.compareTo(o2.state);
+				} else if(field.equals("zip")){
+					currentScore = o1.zip.compareTo(o2.zip);
+				} else if(field.equals("phone1")){
+					currentScore = o1.phone1.compareTo(o2.phone1);
+				} else if(field.equals("phone2")){
+					currentScore = o1.phone2.compareTo(o2.phone2);
+				} else if(field.equals("email")){
+					currentScore = o1.email.compareTo(o2.email);
+				} else if(field.equals("web")){
+					currentScore = o1.web.compareTo(o2.web);
 				}
 				
 				if (currentScore != 0) {
